9929749694bd750968c3de135bac38784783e198
Charles Painter <cpainter85@gmail.com>
2015-04-27 22:36:01 -0600

    user sign up spec passes

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email, :password))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to pokedex_path, notice: 'You have successfully signed up!'
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get 'pokedex', to: 'pokedex#index'
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
