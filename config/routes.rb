499ec3a4e2e77d51fd4fb50ed66261df10b51930
Charles Painter <cpainter85@gmail.com>
2015-05-23 14:46:57 -0600

    user sign up spec passes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+  get 'sign-up', to: 'registrations#new'
+  post 'sign-up', to: 'registrations#create'
+  get 'pokedex', to: 'pokedex#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
