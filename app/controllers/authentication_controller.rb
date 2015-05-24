5fba40e584f6e75900a33d89f5c321fecedcc17b
Charles Painter <cpainter85@gmail.com>
2015-05-24 13:13:45 -0600

    user sign in spec passes

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,15 @@
+class AuthenticationController < ApplicationController
+  def new
+
+  end
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to pokedex_path, notice: 'You have successfully signed in!'
+    else
+      flash[:notice] = 'Invalid User/Password combination'
+      render :new
+    end
+  end
+end
