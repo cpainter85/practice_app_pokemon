8563c172409393b2bbb01427e94842d159a541f0
Charles Painter <cpainter85@gmail.com>
2015-05-24 13:55:36 -0600

    trainer spec passes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'pokedex', to: 'pokedex#index'
 
   resources :pokemons, except: [:index]
+  resources :trainers, except: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
